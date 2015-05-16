58f94808aad5275679c5338ee5e2f3c9db6a1c39
csawtelle <csawtelle@live.com>
2015-05-16 14:59:14 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,87 +4,86 @@ typedef enum { //enumeration is a numbered list of variables
 } sharedHandleId_t;
 #define quesize 100
 
- class readController : public scheduler_task { //activate on the XBEE SJone only
-    public:
-        readController(uint8_t priority) : scheduler_task("readController", 2048, priority) {
-        QueueHandle_t q = xQueueCreate(1, quesize); //create queue
-        addSharedObject(controller_Q, q); //add my sensor to the queue
-    };
-    bool run(void *p) {
-        TickType_t xLastWakeTime;
-        const TickType_t xFrequency = 210;
-        xLastWakeTime = xTaskGetTickCount();
-        char value[quesize] = {0};
-        Uart3& u3 = Uart3::getInstance();
-        u3.init(9600); //baud rate
-        u3.gets(&value[0],quesize,100);
-        puts(value);
-        xQueueSend(getSharedObject(controller_Q), &value, portMAX_DELAY); //sends the data to the queue to be processed
-        vTaskDelayUntil( &xLastWakeTime, xFrequency/portTICK_PERIOD_MS );
-        return true;
-    }
+class readController : public scheduler_task { //activate on the XBEE SJone only
+   public:
+     readController(uint8_t priority) : scheduler_task("readController", 2048, priority) {
+     QueueHandle_t q = xQueueCreate(1, quesize); //create queue
+     addSharedObject(controller_Q, q); //add my sensor to the queue
+   };
+   bool run(void *p) {
+     TickType_t xLastWakeTime;
+     const TickType_t xFrequency = 210;
+     xLastWakeTime = xTaskGetTickCount();
+     char value[quesize] = {0};
+     Uart3& u3 = Uart3::getInstance();
+     u3.init(9600); //baud rate
+     u3.gets(&value[0],quesize,100);
+     puts(value);
+     xQueueSend(getSharedObject(controller_Q), &value, portMAX_DELAY); //sends the data to the queue to be processed
+     vTaskDelayUntil( &xLastWakeTime, xFrequency/portTICK_PERIOD_MS );
+     return true;
+   }
 };
 
- class processController : public scheduler_task //activate on the XBEE controller only
- {
-     public:
-         processController(uint8_t priority) : scheduler_task("processController", 2048, priority) { }
-         bool run(void *p) {
-             char valuebuffer[quesize] = {0};
-             QueueHandle_t q_name = getSharedObject(controller_Q); //create the q
-             if (xQueueReceive(q_name, &valuebuffer, portMAX_DELAY)) {
-                printf("\n");
-                printf(valuebuffer);
-                printf("\n");
- 
-                }
-             return true;
-         }
- };
+class processController : public scheduler_task //activate on the XBEE controller only
+{
+  public:
+      processController(uint8_t priority) : scheduler_task("processController", 2048, priority) { }
+      bool run(void *p) {
+          char valuebuffer[quesize] = {0};
+          QueueHandle_t q_name = getSharedObject(controller_Q); //create the q
+          if (xQueueReceive(q_name, &valuebuffer, portMAX_DELAY)) {
+             printf("\n");
+             printf(valuebuffer);
+             printf("\n");
+
+             }
+          return true;
+      }
+};
 
 class calculateIMU : public scheduler_task //create a task on the AHREF board only
- {
-     public:
-         calculateIMU(uint8_t priority) : scheduler_task("calculate_heading", 2048, priority) { }
-         bool run(void *p) {
-             char valuebuffer[quesize] = {0};
-             char * tokens[3];
-             uint8_t synch_counter = 0;
-             uint8_t buffer_index = 0;
-             float yaw=0;
-             float pitch=0;
-             float roll=0;
-             size_t maxTokens = 0;
-             const char hashtag = '#';
-             QueueHandle_t q_name = getSharedObject(IMU_Q); //create the q
-             if (xQueueReceive(q_name, &valuebuffer, portMAX_DELAY)) {
-                printf("\n");
-                
-                char *tokens[4];
-                char *p;
-                p = strtok(valuebuffer,",");
-                int j = 0;
-                
-                while (p) {
-                    tokens[j++] = p;
-                    p = strtok(NULL, ",");
-                }
-                
-                yaw = str::toFloat(tokens[1]);
-                pitch = str::toFloat(tokens[2]);
-                roll = str::toFloat(tokens[3]);
-                
-                printf("\n");
-                printf("\nYAW:%.3f",yaw);
-                printf("     ");
-                printf("PITCH:%.3f",pitch);
-                printf("     ");
-                printf("ROLL:%.3f",roll);
- 
+{
+  public:
+      calculateIMU(uint8_t priority) : scheduler_task("calculate_heading", 2048, priority) { }
+      bool run(void *p) {
+          char valuebuffer[quesize] = {0};
+          char * tokens[3];
+          uint8_t synch_counter = 0;
+          uint8_t buffer_index = 0;
+          float yaw=0;
+          float pitch=0;
+          float roll=0;
+          size_t maxTokens = 0;
+          const char hashtag = '#';
+          QueueHandle_t q_name = getSharedObject(IMU_Q); //create the q
+          if (xQueueReceive(q_name, &valuebuffer, portMAX_DELAY)) {
+             printf("\n");
+             
+             char *tokens[4];
+             char *p;
+             p = strtok(valuebuffer,",");
+             int j = 0;
+
+             while (p) {
+                 tokens[j++] = p;
+                 p = strtok(NULL, ",");
              }
-             return true;
-         }
- };
+             yaw = str::toFloat(tokens[1]);
+             pitch = str::toFloat(tokens[2]);
+             roll = str::toFloat(tokens[3]);
+
+             printf("\n");
+             printf("\nYAW:%.3f",yaw);
+             printf("     ");
+             printf("PITCH:%.3f",pitch);
+             printf("     ");
+             printf("ROLL:%.3f",roll);
+          }
+          return true;
+      }
+   
+};
  
 class readIMU : public scheduler_task { //create task on AHREF board only
     public:
@@ -107,7 +106,8 @@ class readIMU : public scheduler_task { //create task on AHREF board only
         vTaskDelayUntil( &xLastWakeTime, xFrequency/portTICK_PERIOD_MS );
         return true;
     }
-};
+}
+
 void initializeMotors (void) {
     I2C2& i2c = I2C2::getInstance(); // Get I2C driver instance
     i2c.init(100);
@@ -161,32 +161,33 @@ void initializeMotors (void) {
             i2c.writeReg(slave, n, 0); //off
             delay_ms(1);
         }
+    }
 }
 
 void motorPWM (void) {
-        printf("\n Starting while \n");
-        scanf("%f", &user_input);
-        printf("\n %f", user_input);
-        duty_cycle = (user_input*4096)/100;
-        printf("\n %f",user_input);
-        printf("\n Duty Cycle: ");
-        printf("%d",duty_cycle);
-        if(duty_cycle) {
-            dc_low_byte = duty_cycle;
-            dc_high_byte = duty_cycle >> 8;
-            for (int n = 6; n < 66;) {
-                i2c.writeReg(slave, n, 0x0); //low byte
-                delay_ms(1);
-                i2c.writeReg(slave, n+1, 0x0); //high byte
-                delay_ms(1);
-                i2c.writeReg(slave, n+2, dc_low_byte); //low byte
-                delay_ms(1);
-                i2c.writeReg(slave, n+3, dc_high_byte); //high byte
-                delay_ms(1);
-                n=n+4;
-            }
-            duty_cycle = 0;
-        }    
+     printf("\n Starting while \n");
+     scanf("%f", &user_input);
+     printf("\n %f", user_input);
+     duty_cycle = (user_input*4096)/100;
+     printf("\n %f",user_input);
+     printf("\n Duty Cycle: ");
+     printf("%d",duty_cycle);
+     if(duty_cycle) {
+         dc_low_byte = duty_cycle;
+         dc_high_byte = duty_cycle >> 8;
+         for (int n = 6; n < 66;) {
+             i2c.writeReg(slave, n, 0x0); //low byte
+             delay_ms(1);
+             i2c.writeReg(slave, n+1, 0x0); //high byte
+             delay_ms(1);
+             i2c.writeReg(slave, n+2, dc_low_byte); //low byte
+             delay_ms(1);
+             i2c.writeReg(slave, n+3, dc_high_byte); //high byte
+             delay_ms(1);
+             n=n+4;
+         }
+         duty_cycle = 0;
+     }    
 }
 int main(void) {
 
