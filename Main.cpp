a0c32b8959ac551f077dafb591b42d4a2549ea6d
csawtelle <csawtelle@live.com>
2015-05-16 19:17:33 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,9 @@
 //Globals for now
-uint16_t duty_cycle = 0;
-uint8_t dc_low_byte = 0;
-char dc_high_byte = 0;
-uint8_t slave = 0x40<<1; // 8 bit addr
-uint8_t mode1_reg = 0x0;
-uint8_t prescale_reg = 0xFE;
-uint8_t prescale_value = 0x80;
-uint8_t sleep = 0x10;
-uint8_t initialize = 0xA1;
+    I2C2& i2c = I2C2::getInstance(); // Get I2C driver instance
+    i2c.init(100);
+    uint8_t slave = 0x40<<1; // 8 bit addr
+
+
 typedef enum {
    yaw,
    pitch,
@@ -123,8 +119,11 @@ class readIMU : public scheduler_task { //create task on AHREF board only
 }
 
 void initializeMotors (void) {
-    I2C2& i2c = I2C2::getInstance(); // Get I2C driver instance
-    i2c.init(100);
+      uint8_t mode1_reg = 0x0;
+      uint8_t prescale_reg = 0xFE;
+      uint8_t prescale_value = 0x80;
+      uint8_t sleep = 0x10;
+      uint8_t initialize = 0xA1;
     //initialize
     i2c.writeReg(slave, mode1_reg, sleep); //sleep
     delay_ms(1);
@@ -168,72 +167,75 @@ void initializeMotors (void) {
     }
 }
 
-void motorPWM (uint8_t axis, uint16_t positivePWM, uint16_t negativePWM) {
-   dcPositive = (positivePWM*4096)/100;
-   dcPositiveLow = dcPositive;
-   dcPositiveHigh = dcPositive >> 8;
-   dcNegative = (negativePWM*4096)/100;
-   dcNegativeLow = dcNegative;
-   dcNegativeHigh = dcNegative >> 8;
-   if(axis == yaw) {
-      //yaw is created by creating a power differential between the CW and CCW motors (oppsite rotation 2,6,10,14)
-       i2c.writeReg(slave, n, 0x0); //low byte
-       delay_ms(1);
-       i2c.writeReg(slave, n+1, 0x0); //high byte
-       delay_ms(1);
-       i2c.writeReg(slave, n+2, dcPositiveLow); //low byte
-       delay_ms(1);
-       i2c.writeReg(slave, n+3, dcPositiveHigh); //high byte
-       delay_ms(1);
-       
-   }
-   if(axis == pitch) {
-       //positive pitch is motor 0
-       i2c.writeReg(slave, 6, 0x0); //low byte
-       delay_ms(1);
-       i2c.writeReg(slave, 7, 0x0); //high byte
-       delay_ms(1);
-       i2c.writeReg(slave, 8, dcPositiveLow); //low byte
-       delay_ms(1);
-       i2c.writeReg(slave, 9, dcPositiveHigh); //high byte
-       delay_ms(1);
-       //positive pitch is motor 8
-       i2c.writeReg(slave, 38, 0x0); //low byte
-       delay_ms(1);
-       i2c.writeReg(slave, 39, 0x0); //high byte
-       delay_ms(1);
-       i2c.writeReg(slave, 40, dcNegativeLow); //low byte
-       delay_ms(1);
-       i2c.writeReg(slave, 41, dcNegativeHigh); //high byte
-       delay_ms(1);
-   }
-   if(axis == roll) {
-       //Positive roll is motor 4
-       i2c.writeReg(slave, 22, 0x0); //low byte
-       delay_ms(1);
-       i2c.writeReg(slave, 23, 0x0); //high byte
-       delay_ms(1);
-       i2c.writeReg(slave, 24, dcPositiveLow); //low byte
-       delay_ms(1);
-       i2c.writeReg(slave, 25, dcPositiveHigh); //high byte
-       delay_ms(1);
-       //Negative roll is motor 12
-       i2c.writeReg(slave, 54, 0x0); //low byte
-       delay_ms(1);
-       i2c.writeReg(slave, 55, 0x0); //high byte
-       delay_ms(1);
-       i2c.writeReg(slave, 56, dcNegativeLow); //low byte
-       delay_ms(1);
-       i2c.writeReg(slave, 57, dcNegativeHigh); //high byte
-       delay_ms(1);
-   }
-   duty_cycle = 0;
+void motorPWM (uint16_t yawPID, uint16_t pitchPID, uint16_t rollPID) {
+/*
+
+      Front = Throttle + PitchPID - YawPID
+      Back = Throttle - PitchPID - YawPID
+      Left = Throttle + RollPID + YawPID
+      Right = Throttle - RollPID + YawPID
+
+*/
+
+//The YAW probably wont work because the blades are all same rotation, needs to be fixed
+   dcFront = (frontThrottle*4096)/100 + pitchPID - yawPID;
+   dcFrontLow = dcFront;
+   dcFrontHigh = dcFront >> 8;
+   
+   dcBack = (backThrottle*4096)/100 + pitchPID - yawPID;
+   dcBackLow = dcBack;
+   dcBackHigh = dcBack >> 8;
+   
+   dcLeft = (leftThrottle*4096)/100 + pitchPID - yawPID;
+   dcLeftLow = dcLeft;
+   dcLeftLow = dcLeft >> 8;
+   
+   dcRight = (rightThrottle*4096)/100 + pitchPID - yawPID;
+   dcRightLow = dcRight;
+   dcRightHigh = dcRight >> 8;
+
+   //Front
+   i2c.writeReg(slave, 6, 0x0); //low byte
+   delay_ms(1);
+   i2c.writeReg(slave, 7, 0x0); //high byte
+   delay_ms(1);
+   i2c.writeReg(slave, 8, dcFrontLow); //low byte
+   delay_ms(1);
+   i2c.writeReg(slave, 9, dcFrontHigh); //high byte
+   delay_ms(1);
+   //back
+   i2c.writeReg(slave, 38, 0x0); //low byte
+   delay_ms(1);
+   i2c.writeReg(slave, 39, 0x0); //high byte
+   delay_ms(1);
+   i2c.writeReg(slave, 40, dcBackLow); //low byte
+   delay_ms(1);
+   i2c.writeReg(slave, 41, dcBackHigh); //high byte
+   delay_ms(1);
+   //left
+   i2c.writeReg(slave, 22, 0x0); //low byte
+   delay_ms(1);
+   i2c.writeReg(slave, 23, 0x0); //high byte
+   delay_ms(1);
+   i2c.writeReg(slave, 24, dcLeftLow); //low byte
+   delay_ms(1);
+   i2c.writeReg(slave, 25, dcLeftHigh); //high byte
+   delay_ms(1);
+   //right
+   i2c.writeReg(slave, 54, 0x0); //low byte
+   delay_ms(1);
+   i2c.writeReg(slave, 55, 0x0); //high byte
+   delay_ms(1);
+   i2c.writeReg(slave, 56, dcRightLow); //low byte
+   delay_ms(1);
+   i2c.writeReg(slave, 57, dcRightHigh); //high byte
+   delay_ms(1);
 }
 int main(void) {
-
+   
+   
+   
     //Add tasks depending on which board you are loading the code too
-
-
     /**
      * A few basic tasks for this bare-bone system :
      *      1.  Terminal task provides gateway to interact with the board through UART terminal.
