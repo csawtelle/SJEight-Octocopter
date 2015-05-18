08b3def7f79070bfae18e124f7f953c40ad8a80e
csawtelle <csawtelle@live.com>
2015-05-17 17:03:37 -0700

    Update Motors.h

--- Motors.h
+++ Motors.h
@@ -1,11 +1,11 @@
-struct motorThrottle
-{
-   uint16_t 
-   uint16_t 
-   uint16_t 
-   uint16_t 
-   uint16_t
-   uint16_t
-   uint16_t
-};
+struct motorThrottle_struct {
+   uint16_t frontCCW;
+   uint16_t frontCW;
+   uint16_t leftCCW;
+   uint16_t leftCW;
+   uint16_t rightCCW;
+   uint16_t rightCW;
+   uint16_t backCCW;
+   uint16_t backCW;
+} motorThrottle; 
  
