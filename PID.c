0081d1f76bc55af221deaa318b1667b7d6133e43
csawtelle <csawtelle@live.com>
2015-05-22 00:25:58 -0700

    Update PID.c

--- PID.c
+++ PID.c
@@ -30,20 +30,20 @@ int16_t pidController(int16_t set, int16_t currentPWM, struct pidDATA *pid) {
   temp = pid_st->sumError + error;
   if(temp > pid_st->maxSumE) {
     iTerm = MAX_I_TERM;
-    pid_st->sumE = pid_st->maxSumE;
+    pid->sumE = pid_st->maxSumE;
   }
   else if(temp < -pid_st->maxSumError) {
     iTerm = -MAX_I_TERM;
-    pid_st->sumE = -pid_st->maxSumE;
+    pid->sumE = -pid_st->maxSumE;
   }
   else {
-    pid_st->sumE = temp;
+    pid->sumE = temp;
     iTerm = pid_st->i * pid_st->sumE;
   }
  
   dTerm = pid_st->d * (pid_st->previousPWM - currentPWM);
  
-  pid_st->previousPWM = currentPWM;
+  pid->previousPWM = currentPWM;
  
   PWM = (pTerm + iTerm + dTerm) / SCALING_FACTOR;
   if(PWM > MAX_INT) {
@@ -56,7 +56,7 @@ int16_t pidController(int16_t set, int16_t currentPWM, struct pidDATA *pid) {
   return((int16_t)PWM);
 }
  
-void pid_flush_I(pidData_t *pid)
+void pid_flush_I(pidData *pid)
 {
-  pid_st->sumE = 0;
+  pid->sumE = 0;
 }
