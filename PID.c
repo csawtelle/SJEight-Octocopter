5185b7f9b63092257dd82ffb59c9d007051c1b35
csawtelle <csawtelle@live.com>
2015-05-23 21:25:55 -0700

    Update PID.c

--- PID.c
+++ PID.c
@@ -1,50 +1,50 @@
 #include "pid.h"
 #include "stdint.h"
- 
+
 void pidInit(int16_t p, int16_t i, int16_t d, struct pidDATA *pid) {
   pid->sumE = 0;
   pid->previousPWM = 0;
-  pid->pGain = p;
-  pid->iGain = i;
-  pid->dGain = d;
-  pid->maxE = MAX_INT / (pid->p + 1);
-  pid->maxsumE = MAX_I_TERM / (pid->i + 1);
+  pid->P = p;
+  pid->I = i;
+  pid->D = d;
+  pid->maxE = MAX_INT / (pid->P + 1);
+  pid->maxSumE = MAX_I_TERM / (pid->I + 1);
 }
- 
-int16_t pidController(int16_t set, int16_t currentPWM, struct pidDATA *pid) {
+
+int16_t pidController(int16_t setPoint, int16_t currentPWM, struct pidDATA *pid) {
   int16_t error, pTerm, dTerm;
   int32_t iTerm, PWM, temp;
- 
-  error = setPoint - currentPWM;
- 
-  if (error > pid_st->maxE) {
+
+  error = (setPoint - currentPWM);
+
+  if (error > pid->maxE) {
     pTerm = MAX_INT;
   }
-  else if (error < -pid_st->maxE) {
+  else if (error < -pid->maxE) {
     pTerm = -MAX_INT;
   }
   else {
-    pTerm = pid_st->p * error;
+    pTerm = pid-> P * error;
   }
- 
-  temp = pid_st->sumError + error;
-  if(temp > pid_st->maxSumE) {
+
+  temp = pid->sumE + error;
+  if(temp > pid->maxSumE) {
     iTerm = MAX_I_TERM;
-    pid->sumE = pid_st->maxSumE;
+    pid->sumE = pid->maxSumE;
   }
-  else if(temp < -pid_st->maxSumError) {
+  else if(temp < -pid->maxSumE) {
     iTerm = -MAX_I_TERM;
-    pid->sumE = -pid_st->maxSumE;
+    pid->sumE = -pid->maxSumE;
   }
   else {
     pid->sumE = temp;
-    iTerm = pid_st->i * pid_st->sumE;
+    iTerm = pid->I * pid->sumE;
   }
- 
-  dTerm = pid_st->d * (pid_st->previousPWM - currentPWM);
- 
+
+  dTerm = pid->D * (pid->previousPWM - currentPWM);
+
   pid->previousPWM = currentPWM;
- 
+
   PWM = (pTerm + iTerm + dTerm) / SCALING_FACTOR;
   if(PWM > MAX_INT) {
     PWM = MAX_INT;
@@ -52,11 +52,11 @@ int16_t pidController(int16_t set, int16_t currentPWM, struct pidDATA *pid) {
   else if(PWM < -MAX_INT) {
     PWM = -MAX_INT;
   }
- 
+
   return((int16_t)PWM);
 }
- 
-void pid_flush_I(pidData *pid)
+
+void pid_flush_I(struct pidDATA *pid)
 {
   pid->sumE = 0;
 }
