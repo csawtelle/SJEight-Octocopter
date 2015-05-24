872ec8ac856b0b28111760a8fc7f184d72e6b5f2
csawtelle <csawtelle@live.com>
2015-05-23 21:26:16 -0700

    Update PID.h

--- PID.h
+++ PID.h
@@ -1,10 +1,10 @@
 #ifndef PID_H
 #define PID_H
- 
+
 #include "stdint.h"
- 
+
 #define SCALING_FACTOR  128
- 
+
 typedef struct pidDATA{
   int16_t previousPWM;
   int32_t sumE;
@@ -24,9 +24,9 @@ struct pidDATA rollPID;
 #define MAX_I_TERM      (MAX_LONG / 2)
 #define FALSE           0
 #define TRUE            1
- 
-void pid_Init(int16_t p, int16_t i, int16_t d, struct pidDATA *pid);
-int16_t pid_Controller(int16_t setPoint, int16_t currentAngle, struct pidDATA *pid);
+
+void pidInit(int16_t p, int16_t i, int16_t d, struct pidDATA *pid);
+int16_t pidController(int16_t setPoint, int16_t currentAngle, struct pidDATA *pid);
 void pid_Reset_Integrator(struct pidDATA *pid);
- 
+
 #endif
